403c5123256a16882e9e01f669d08452de4f8c9a
sanny gupta <sannygupta79@gmail.com>
2024-08-30 09:15:30 +0530

    first commit

---
+++ food/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def sweet(request):
+    return render(request,'sweet.html')
+
---
+++ travel/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def bihar(request):
+    return render(request,'bihar.html')
